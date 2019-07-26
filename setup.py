aea221b940e80b0005806bf8ae0da793f23fe119
Dane Finlay <Danesprite@posteo.net>
2019-07-26 18:35:40 +1000

    Add post-install code to register the DLL in setup.py
    This uses the Windows ShellExecute function to register the server
    with administrative privileges. The UAC prompt and regsrv32
    success/failure dialog windows will be shown.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 r"""A MSAA client library.
 
 """
+
+import ctypes
+import os
+import sys
+
 from setuptools import setup
 import pyia2
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 classifiers = [
     'Development Status :: 3 - Alpha',
@@ -12,6 +19,41 @@ classifiers = [
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules',
     ]
+
+
+def post_install():
+    # Display a message about registering the DLL.
+    print("Attempting to register IAccessible2Proxy.dll with elevated "
+          "privileges...")
+
+    # Do this via the Windows ShellExecuteA / ShellExecuteW functions.
+    # Use the ANSI function if using Python 2.
+    shell32 = ctypes.windll.shell32
+    PY2 = sys.version_info[0] == 2
+    ShellExecute = shell32.ShellExecuteA if PY2 else shell32.ShellExecuteW
+
+    # Get the DLL's path and attempt to register it with elevated privileges.
+    dll_path = os.path.join(os.getcwd(), "pyia2", "IAccessible2Proxy.dll")
+    return_code = ShellExecute(None, "runas", "regsvr32.exe", dll_path, None, 1)
+    if return_code > 32:
+        print("Success.")
+    else:
+        print("Failed to register the DLL. It must be registered manually.")
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        post_install()
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        post_install()
+        install.run(self)
+
 
 setup(name="pyia2",
       description="Python MSAA+IAccessible2 client library",
@@ -25,4 +67,8 @@ setup(name="pyia2",
       version=pyia2.__version__,
       packages=["pyia2"],
       package_data={"": ["*.tlb"]},
-      install_requires=["comtypes"])
+      install_requires=["comtypes"],
+      cmdclass={
+          'develop': PostDevelopCommand,
+          'install': PostInstallCommand,
+      })
