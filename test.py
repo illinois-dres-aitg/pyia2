fe6f96a3aabc6e4efe6ac5865ad1be0a7804b66c
Dane Finlay <Danesprite@posteo.net>
2019-11-19 16:20:20 +1100

    Change test scripts to work with Python 2.7 and Python 3

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-import pyia2
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+
+import six
+
+import pyia2
+
+if six.PY2:
+  reload(sys)
+  sys.setdefaultencoding('utf-8')
 
 desktop = pyia2.getDesktop()
-print str(desktop)
+print(str(desktop))
 
 for window in desktop:
   if not window.accState(0) & pyia2.STATE_SYSTEM_INVISIBLE:
-     print repr(str(window))
+     print(repr(str(window)))
 
 # Need to get ia2.tlb
 # create a module comptypes.client.GetModule(path to ia2.tlb)
